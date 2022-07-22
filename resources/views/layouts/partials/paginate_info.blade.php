79972fe2eab9a41b60c8220475e4027ef140072f
Babul Mirdha <babumirdha@gmail.com>
2022-07-22 20:48:47 +0600

    deleted.

--- resources/views/layouts/partials/paginate_info.blade.php
+++
@@ -1,8 +0,0 @@
-{{-- <div class="d-flex justify-content-between mt-2"> --}}
-
-    @if (!is_array($paginator))
-        <div class="p-2">
-            {!! $paginator->links() !!}
-        </div>
-    @endif
-{{-- </div> --}}
