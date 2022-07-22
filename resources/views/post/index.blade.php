adf176e3532006b895e724aa538dc42fef1fd469
Babul Mirdha <babumirdha@gmail.com>
2022-07-22 23:13:53 +0600

    show post in welcome screen.

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -14,35 +14,8 @@
                 <a href="{{ route('posts.create') }}" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-full">Create</a>
             @endpermission
 
-            <div class="p-2">
-                {!! $posts->links() !!}
-            </div>
+            @include('post.post_list_view')
 
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-
-
-
-                @forelse($posts as $post)
-                    <div class="p-6 m-6 bg-white border-b border-gray-200">
-                        {{ $post->post_title }}
-
-                        <a href="{{ route('posts.show', $post->id) }}">Details</a>
-
-                        @permission('post_edit')
-                            <a href="{{ route('posts.edit', $post->id) }}">Edit</a>
-                        @endpermission
-
-
-                    </div>
-
-                @empty
-                    <p>No Posts</p>
-                @endforelse
-            </div>
-
-            <div class="p-2">
-                {!! $posts->links() !!}
-            </div>
         </div>
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\OptionController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = Post::latest()->paginate();
+    return view('welcome', compact('posts') );
 })->name("/");
 
 Route::get('/dashboard', function () {
