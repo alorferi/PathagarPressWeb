e14e525c4744cfdff518edb0db8e889fb2deb623
Babul Mirdha <babumirdha@gmail.com>
2022-10-24 17:59:13 +0600

    Improved card for admin post.

--- resources/views/admin/post/post_horizontal_card_item.blade.php
+++ resources/views/admin/post/post_horizontal_card_item.blade.php
@@ -1,23 +1,73 @@
-<div class="max-w-sm w-full lg:max-w-full lg:flex">
-  <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
-  </div>
-  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    <div class="mb-8">
-      <p class="text-sm text-gray-600 flex items-center">
-        <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-        </svg>
-        Members only
-      </p>
-      <div class="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-      <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+<div class="max-w-sm w-full lg:max-w-full lg:flex py-2 px-4">
+    @if ($post->image != null)
+        <img class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+            src="{{ asset($post->image->url) }}" width="256" />
+    @endif
+
+    <div
+        class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div class="mb-8">
+
+
+
+            <div class="text-gray-900 font-bold text-xl mb-2"> {{ $post->post_title }}</div>
+            <p class="text-gray-700 text-base"> {{ $post->post_content }}</p>
+        </div>
+        @if ($post->author != null)
+            <div class="flex items-center">
+                @if ($post->author->image)
+                    <img class="w-10 h-10 rounded-full mr-4" src="{{ $post->author->image->url }}"
+                        alt="Avatar of Jonathan Reinink">
+                @endif
+                <div class="text-sm">
+                    <p class="text-gray-900 leading-none">{{ $post->author->name }}</p>
+                    <p class="text-gray-600">{{ $post->post_date->diffForhumans() }}</p>
+                </div>
+            </div>
+        @endif
+
+        <a class="bg-blue-500 text-white font-bold py-2 px-4"
+            href="{{ route('posts.show', $post->id) }}" target="_blank">Details</a>
+        &nbsp;
+        @permission('post_edit')
+            <a class="bg-blue-500 text-white font-bold py-2 px-4" href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
+        @endpermission
+
     </div>
-    <div class="flex items-center">
-      <img class="w-10 h-10 rounded-full mr-4" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink">
-      <div class="text-sm">
-        <p class="text-gray-900 leading-none">Jonathan Reinink</p>
-        <p class="text-gray-600">Aug 18</p>
-      </div>
-    </div>
-  </div>
 </div>
+
+
+  {{-- <div class="p-6 m-6 bg-white border-b border-gray-200">
+
+        @if ($post->image != null)
+            <img src="{{ asset($post->image->url) }}" width="258" height="100" />
+        @endif
+
+        {{ $post->post_title }}
+
+
+        @if ($post->author != null)
+            <div class="px-6 pt-4 pb-2 flex items-center">
+
+                @if ($post->author->image)
+                    <img class="w-10 h-10 rounded-full mr-6" src="{{ $post->author->image->url }}"
+                        alt="Avatar of Jonathan Reinink" />
+                @endif
+                <div class="text-sm">
+                    <p class="text-gray-900 leading-none">{{ $post->author->name }}</p>
+                    <p class="text-gray-600">{{ $post->post_date->diffForhumans() }}</p>
+                </div>
+            </div>
+        @endif
+
+
+        <a class="bg-blue-500 text-white font-bold py-2 px-4"class="bg-blue-500 text-white font-bold py-2 px-4"
+            href="{{ route('posts.show', $post->id) }}" target="_blank">Details</a>
+
+        &nbsp;
+        @permission('post_edit')
+            <a class="bg-blue-500 text-white font-bold py-2 px-4" href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
+        @endpermission
+
+
+    </div> --}}
